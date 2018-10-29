c3430ba217b8dbd7c73cce3473d3cb868258888f
NarrowWindow <ochoad64@gmail.com>
2018-10-29 13:20:43 -0500

    Made psueudocode as comments in the main function

--- BullsAndCowsCPP/BullsAndCowsCPP/BullsAndCowsCPP.cpp
+++ BullsAndCowsCPP/BullsAndCowsCPP/BullsAndCowsCPP.cpp
@@ -15,6 +15,9 @@ void OneGame(string word);
 // Array of words for the player to be able to guess
 string words[] = { "girl", "calf", "verb", "duck" };
 
+// Number of guesses the player has until they lose
+int MaxGuesses = 6;
+
 int main()
 {
 	// Get a random word from the array and send it to the main game function
@@ -26,13 +29,36 @@ int main()
 
 void OneGame(string word)
 {
-	// Display main menu
+	int NumberOfGuesses = 0;
 
-	// Prompt user for input and tell them how to exit the game (enter -1)
-	string guess;
-	cout << "Enter your guess: ";
-	cin >> guess;
-	cout << "You entered: " << guess << endl;
+	while (NumberOfGuesses < MaxGuesses)
+	{
+		// Display main menu
+
+		// Prompt user for input and tell them how to exit the game (enter -1)
+		string guess;
+		cout << "Enter your guess (Enter -1 to exit the game): ";
+		cin >> guess;
+
+		// Check if they entered -1
+
+		// Check if the input is invalid and send back to input prompt if it is
+
+		// Count number of bulls
+		int bulls = findBulls(word, guess);
+
+		// Counter number of cows
+		int cows = findCows(word, guess);
+
+		// Remove the false cows that are actually bulls
+		cows -= bulls;
+
+		// Display number of bulls and cows
+
+		// Check if the player has won using a function
+
+		// If they did win, return and exit this function
+	}	
 }
 
 int findBulls(string secret, string guess)
